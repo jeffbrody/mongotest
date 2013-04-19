04dc51e16d8ccad5f96de225672c8573d345fc9f
Jeff Brody <jeff@bluecrowconsulting.com>
2013-04-19 10:21:11 -0600

    Seventy site without Activerecord

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seventy::Application.config.secret_token = 'aec14f95ffb2078ad0a69fdc4978fbbafb0a0ab54e5fa4f14bbbbbe35ef6e41af8faeeb7daeba859afbf887c1da96ef00903cb0e09929ee42b74264358e3dec6'
